8ddd4bb7b594a38a0afdc5e4aa8d11cfe0cbf51f
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-04 17:29:52 +0200

    Update trie.rs

--- crates/eir-core/src/index/trie.rs
+++ crates/eir-core/src/index/trie.rs
@@ -1,25 +1,61 @@
+use rkyv::{Archive, Deserialize, Serialize};
+
 use std::collections::HashMap;
 
 use crate::entity::types::EntityID;
 
+use super::utils::normalize;
+
 #[derive(Default, Debug)]
 pub struct TrieIndex {
-    entries: HashMap<String, Vec<EntityID>>,
+    root: HashMap<Box<str>, Vec<EntityID>>,
 }
 
 impl TrieIndex {
     pub fn insert(&mut self, text: &str, entity_id: EntityID) {
-        self.entries
-            .entry(text.to_lowercase())
+        self.root
+            .entry(normalize(text))
             .or_default()
             .push(entity_id);
     }
 
     pub fn prefix(&self, prefix: &str) -> Vec<EntityID> {
-        self.entries
+        self.root
             .iter()
             .filter(|(key, _)| key.starts_with(prefix))
             .flat_map(|(_, ids)| ids.clone())
             .collect()
+    }
+
+    pub fn export(&self) -> Vec<(Box<str>, Vec<EntityID>)> {
+        self.root
+            .iter()
+            .map(|(key, value)| (key.clone(), value.clone()))
+            .collect()
+    }
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug, Clone, Default)]
+pub struct TrieIndexRecord {
+    pub entries: Vec<(Box<str>, Vec<EntityID>)>,
+}
+
+impl TrieIndex {
+    pub fn to_record(&self) -> TrieIndexRecord {
+        TrieIndexRecord {
+            entries: self.export(),
+        }
+    }
+
+    pub fn from_record(record: TrieIndexRecord) -> Self {
+        let mut trie = Self::default();
+
+        for (word, ids) in record.entries {
+            for id in ids {
+                trie.insert(&word, id);
+            }
+        }
+
+        trie
     }
 }
