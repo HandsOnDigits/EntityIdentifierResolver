e93e4fac8146a698a3694158cdd2dfc8f0c41b7f
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-04 17:29:58 +0200

    Update inverted.rs

--- crates/eir-core/src/index/inverted.rs
+++ crates/eir-core/src/index/inverted.rs
@@ -1,24 +1,47 @@
+use rkyv::{Archive, Deserialize, Serialize};
+
 use std::collections::HashMap;
 
 use crate::entity::types::EntityID;
 
-#[derive(Default, Debug)]
+use super::utils::normalize;
+
+#[derive(Default, Debug, Clone)]
 pub struct InvertedIndex {
-    terms: HashMap<String, Vec<EntityID>>,
+    pub index: HashMap<Box<str>, Vec<EntityID>>,
 }
 
 impl InvertedIndex {
     pub fn insert(&mut self, term: &str, entity_id: EntityID) {
-        self.terms
-            .entry(term.to_lowercase())
+        self.index
+            .entry(normalize(term))
             .or_default()
             .push(entity_id);
     }
 
     pub fn lookup(&self, term: &str) -> Vec<EntityID> {
-        self.terms
-            .get(&term.to_lowercase())
+        self.index
+            .get(&normalize(term))
             .cloned()
             .unwrap_or_default()
+    }
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug, Clone, Default)]
+pub struct InvertedIndexRecord {
+    pub entries: HashMap<Box<str>, Vec<EntityID>>,
+}
+
+impl InvertedIndex {
+    pub fn to_record(&self) -> InvertedIndexRecord {
+        InvertedIndexRecord {
+            entries: self.index.clone(),
+        }
+    }
+
+    pub fn from_record(record: InvertedIndexRecord) -> Self {
+        Self {
+            index: record.entries,
+        }
     }
 }
