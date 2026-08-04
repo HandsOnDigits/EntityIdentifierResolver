c134bfdf9dc98f8d756ef6a6cb233d47ce7e2e12
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-04 17:30:04 +0200

    Update database.rs

--- crates/eir-core/src/engine/database.rs
+++ crates/eir-core/src/engine/database.rs
@@ -3,23 +3,24 @@ use rkyv::{Archive, Deserialize, Serialize, bytecheck::CheckBytes};
 use crate::{
     entity::{
         EntityDocument,
-        types::{EntityID, SourceID, TagID},
+        types::{SourceID, TagID},
     },
+    index::{AliasIndexRecord, BKTreeIndexRecord, InvertedIndexRecord, TrieIndexRecord},
     storage::PostingListRecord,
 };
-
-use std::collections::HashMap;
 
 #[derive(Debug, Archive, Serialize, Deserialize, CheckBytes)]
 pub struct Database {
     pub entities: Vec<EntityDocument>,
 
-    // entity_id -> aliases
-    pub aliases: HashMap<EntityID, Vec<Box<str>>>,
-
     pub tags: Vec<Box<str>>,
     pub sources: Vec<Box<str>>,
     pub properties: Vec<Box<str>>,
+
+    pub alias_index: AliasIndexRecord,
+    pub trie_index: TrieIndexRecord,
+    pub bk_tree_index: BKTreeIndexRecord,
+    pub inverted_index: InvertedIndexRecord,
 
     pub tag_index: PostingListRecord<TagID>,
     pub source_index: PostingListRecord<SourceID>,
