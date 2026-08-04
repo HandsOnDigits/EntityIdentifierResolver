a63840dab4b6fc49af9cb7f1f29a5f3966a7e4a9
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-04 17:30:02 +0200

    Update alias.rs

--- crates/eir-core/src/index/alias.rs
+++ crates/eir-core/src/index/alias.rs
@@ -1,17 +1,19 @@
+use rkyv::{Archive, Deserialize, Serialize};
+
 use std::collections::HashMap;
 
 use crate::{entity::types::EntityID, index::utils::normalize};
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct AliasIndex {
-    aliases: HashMap<String, Vec<EntityID>>,
+    index: HashMap<Box<str>, Vec<EntityID>>,
 }
 
 impl AliasIndex {
-    pub fn insert(&mut self, alias: impl Into<String>, entity_id: EntityID) {
+    pub fn insert(&mut self, alias: impl Into<Box<str>>, entity_id: EntityID) {
         let key = normalize(&alias.into());
 
-        let ids = self.aliases.entry(key).or_default();
+        let ids = self.index.entry(key).or_default();
 
         if !ids.contains(&entity_id) {
             ids.push(entity_id);
@@ -19,8 +21,19 @@ impl AliasIndex {
     }
 
     pub fn resolve(&self, alias: &str) -> Option<&[EntityID]> {
-        self.aliases
-            .get(&normalize(alias))
-            .map(|ids| ids.as_slice())
+        self.index.get(&normalize(alias)).map(|ids| ids.as_slice())
+    }
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug, Clone)]
+pub struct AliasIndexRecord {
+    pub entries: HashMap<Box<str>, Vec<EntityID>>,
+}
+
+impl AliasIndex {
+    pub fn to_record(&self) -> AliasIndexRecord {
+        AliasIndexRecord {
+            entries: self.index.clone(),
+        }
     }
 }
