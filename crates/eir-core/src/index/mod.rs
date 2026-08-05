db8ff5dd3085514e62ae8581abc8589edc06f49d
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-05 12:00:01 +0200

    Update mod.rs

--- crates/eir-core/src/index/mod.rs
+++ crates/eir-core/src/index/mod.rs
@@ -1,6 +1,7 @@
 mod alias;
 mod bk_tree;
 mod inverted;
+mod ranker;
 mod resolver;
 mod trie;
 pub mod utils;
@@ -8,6 +9,7 @@ pub mod utils;
 pub use alias::{AliasIndex, AliasIndexRecord};
 pub use bk_tree::{BKTreeIndex, BKTreeIndexRecord};
 pub use inverted::{InvertedIndex, InvertedIndexRecord};
+pub use ranker::{Ranker, SearchHit};
 pub use resolver::{Resolver, SearchResult};
 pub use trie::{TrieIndex, TrieIndexRecord};
 
