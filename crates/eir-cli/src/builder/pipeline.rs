c84ea28023c477619a4a4b069df32bcc45fb73fb
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-05 20:23:32 +0200

    Update pipeline.rs

--- crates/eir-cli/src/builder/pipeline.rs
+++ crates/eir-cli/src/builder/pipeline.rs
@@ -29,6 +29,8 @@ pub fn build(
         bk_tree_index: indexes.bk_tree.to_record(),
         inverted_index: indexes.inverted.to_record(),
 
+        attribute_index: indexes.attributes.to_record(),
+
         tag_index: indexes.tags.to_record(),
         source_index: indexes.sources.to_record(),
     };
