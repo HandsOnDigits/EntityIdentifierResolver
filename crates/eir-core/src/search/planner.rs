d3c81fbdf43ba6d5af5f75ee6d4a128b236b002e
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-06 16:44:50 +0200

    Update planner.rs

--- crates/eir-core/src/search/planner.rs
+++ crates/eir-core/src/search/planner.rs
@@ -1,6 +1,6 @@
 use crate::query::{Query, QueryIntent};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum SearchStage {
     ExactAlias,
     PrefixAlias,
@@ -23,6 +23,7 @@ impl SearchPlan {
                 SearchStage::ExactAlias,
                 SearchStage::PrefixAlias,
                 SearchStage::FuzzyAlias { distance: 1 },
+                SearchStage::Token,
             ],
 
             QueryIntent::Browse => vec![SearchStage::Tag, SearchStage::Token],
@@ -33,9 +34,48 @@ impl SearchPlan {
 
             QueryIntent::Similar => vec![SearchStage::Tag, SearchStage::Attribute],
 
+            QueryIntent::Tag => vec![SearchStage::Tag],
+
             QueryIntent::Unknown => vec![SearchStage::Token],
         };
 
         Self { stages }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::query::{Query, QueryIntent};
+
+    #[test]
+    fn lookup_creates_lookup_plan() {
+        let query = Query {
+            original: "FizzBerry".into(),
+            normalized: "fizzberry".into(),
+            tokens: vec!["fizzberry".into()],
+            intent: QueryIntent::Lookup,
+            filters: Vec::new(),
+        };
+
+        let plan = SearchPlan::from_query(&query);
+
+        assert!(plan.stages.contains(&SearchStage::Token));
+        assert!(plan.stages.contains(&SearchStage::ExactAlias));
+    }
+
+    #[test]
+    fn tag_creates_tag_plan() {
+        let query = Query {
+            original: "tag:drink".into(),
+            normalized: "tag:drink".into(),
+            tokens: vec!["drink".into()],
+            intent: QueryIntent::Tag,
+            filters: Vec::new(),
+        };
+
+        let plan = SearchPlan::from_query(&query);
+
+        assert_eq!(plan.stages, vec![SearchStage::Tag]);
     }
 }
