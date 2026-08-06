13ea2ae3fa259e39a8493500df44a66a482f9777
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-06 11:52:15 +0200

    Reordered some types.

--- crates/eir-core/src/query/mod.rs
+++ crates/eir-core/src/query/mod.rs
@@ -1,17 +1,9 @@
 mod filters;
 mod intent;
-pub mod parser;
-pub mod types;
+mod parser;
+mod types;
 
-pub use filters::Filter;
-pub use intent::QueryIntent;
-
-use types::*;
-
-pub struct Query {
-    pub original: Message,
-    pub normalized: Message,
-    pub tokens: Vec<Token>,
-    pub intent: QueryIntent,
-    pub filters: Vec<Filter>,
-}
+pub use filters::*;
+pub use intent::*;
+pub use parser::*;
+pub use types::*;
--- crates/eir-core/src/query/types.rs
+++ crates/eir-core/src/query/types.rs
@@ -1,2 +1,12 @@
+use super::{Filter, QueryIntent};
+
 pub type Token = Box<str>;
 pub type Message = Box<str>;
+
+pub struct Query {
+    pub original: Message,
+    pub normalized: Message,
+    pub tokens: Vec<Token>,
+    pub intent: QueryIntent,
+    pub filters: Vec<Filter>,
+}
