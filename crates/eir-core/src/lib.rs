ddebfb0e0ed2581aa5b6fd02287c833aa50a76d7
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-07 12:15:43 +0200

    Update lib.rs

--- crates/eir-core/src/lib.rs
+++ crates/eir-core/src/lib.rs
@@ -2,7 +2,7 @@ pub mod config;
 pub mod engine;
 pub mod entity;
 pub mod error;
-pub mod index;
+mod index;
 pub mod query;
 pub mod search;
 pub mod storage;
@@ -12,10 +12,19 @@ pub use engine::Database;
 pub use storage::{IndexBuilder, Indexes};
 
 pub mod prelude {
-    pub use crate::{
-        engine::{Database, Engine, EngineBuilder},
-        entity::types::*,
-        entity::*,
-        storage::{IndexBuilder, Indexes, PostingList, PostingListRecord, Registry, Store},
-    };
+    pub use super::entity;
+
+    pub mod engine {
+        pub use crate::engine::{Database, Engine, EngineBuilder};
+    }
+
+    pub mod index {
+        pub use crate::index::*;
+    }
+
+    pub mod storage {
+        pub use crate::storage::{
+            IndexBuilder, Indexes, PostingList, PostingListRecord, Registry, RegistryRecord, Store,
+        };
+    }
 }
